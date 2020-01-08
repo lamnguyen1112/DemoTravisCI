308ac7457cb38cf82b3353b998ec605ff72ba9a4
Nguyen Tien Lam <lam.nt@altplus.com.vn>
2020-01-08 13:05:04 +0700

    comit

---
+++ DemoCI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoCI
+//
+//  Created by Nguyen Tien LAM on 1/8/20.
+//  Copyright © 2020 Nguyen Tien LAM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
